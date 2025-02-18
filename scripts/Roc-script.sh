22f82d2b81a4d5d5bb15c212e8bf8c4b5ba6b740
Gabriel Chan <36693835+chanbaihai@users.noreply.github.com>
2025-02-18 09:41:08 +0800

    删除无线控制器

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -24,7 +24,6 @@ git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-ali
 git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
 git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
 git_sparse_clone master https://github.com/immortalwrt/packages net/ariang
-git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
