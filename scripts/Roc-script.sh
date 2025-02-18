139ba5066ad6569d856437e7ab2c508b87e764a0
Gabriel Chan <36693835+chanbaihai@users.noreply.github.com>
2025-02-18 10:12:47 +0800

    add openclash

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -24,6 +24,7 @@ git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-ali
 git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
 git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
 git_sparse_clone master https://github.com/immortalwrt/packages net/ariang
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
